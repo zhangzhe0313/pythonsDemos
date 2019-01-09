fed99eb64f4336ab96f6aada8a3754fea7995447
zhangzhe <zhangzhe@minshenglife.com>
2019-01-09 18:14:32 +0800

    逻辑修改-未完成

--- socket/uploadServer.py
+++ socket/uploadServer.py
@@ -13,25 +13,35 @@ ip_port = ('127.0.0.1', 8888)
 sk.bind(ip_port)
 # 设置最大连接数
 sk.listen(5)
-# 循环
-while True:
-    msg = '服务器已连接'
-    conn, adress = sk.accept()
-    conn.send(msg.encode())
-    while True:
-        # 捕获异常
-        try:
-            f = open('testFile.txt', 'ab')
-        # 捕获异常类型：如果不加类型则会捕获所有类型，若使用Exception 则会捕获除了与程序退出相关的其他异常
-        except IOError as err:
-            print('文件打开失败：%s' % (err))
-        # 若是try中执行未出错，则执行else
-        else:
-            data = conn.recv(1024)
-            f.write(data)
-        # 最终都会执行
-        finally:
-            f.close()
-        conn.send('success'.encode())
+#
+msg = '服务器已连接'
+conn, adress = sk.accept()
+conn.send(msg.encode())
+# 接受到客户端准备就绪信号
+info = conn.recv(1024)
+if info == b'onReady':
+    print('客户端准备就绪: %s ' % (info.decode()))
+    try:
+        with open('testFile.txt', 'ab') as f:
+            # 成功打开文件，发送确认消息
+            sure = 'ACK'
+            conn.send(sure.encode())
+            # 开始接受客户端发送文件信息
+            while True:
+                data = conn.recv(1024)
+                if data == b'finish':
+                    break
+                else:
+                    f.write(data)
+        conn.close()
+        print('服务器接收完成')
+    # 捕获异常类型：如果不加类型则会捕获所有类型，若使用Exception 则会捕获除了与程序退出相关的其他异常
+    except Exception as err:
+        print('服务器文件打开失败：%s' % (err))
+        sure = 'onFail'
+        conn.send(sure.encode())
+        conn.close()
+else:
+    print('客户端准备失败: %s ' % (info.decode()))
     conn.close()
 sk.close()
