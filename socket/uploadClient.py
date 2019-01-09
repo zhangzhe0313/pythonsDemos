b0ce3914efa72d0f7df9d2049c42961f89dbb5fe
zhangzhe <zhangzhe@minshenglife.com>
2019-01-09 18:14:12 +0800

    逻辑修改-未完成

--- socket/uploadClient.py
+++ socket/uploadClient.py
@@ -13,16 +13,31 @@ ip_port = ('127.0.0.1', 8888)
 sk.connect(ip_port)
 data = sk.recv(1024)
 print(data.decode())
+# 捕获异常
 try:
-    f = open('tcpClient.py', 'rb')
-except IOError as err:
-    print(err)
-else:
-    for i in f:
-        sk.send(i)
-        data = sk.recv(1024)
-        if data != b'success':
-            break
-finally:
-    f.close()
-sk.close()
+    # with 执行完，自动关闭文件
+    with open('tcpClient.py', 'rb') as f:
+        # 未出错，发送准备就绪消息
+        msg = 'onReady'
+        sk.send(msg.encode())
+        # 接受服务器返回的确认信息
+        svBack = sk.recv(1024)
+        # 服务器确认就绪
+        if svBack == b'ACK':
+            print('服务器已准备就绪: %s' % (svBack.decode()))
+            for i in f:
+                sk.send(i)
+            # 关闭连接
+            finish = 'finish'
+            sk.send(finish.encode())
+            sk.close()
+            print('客户端已上传完成')
+        # 服务器未确认就绪
+        else:
+            print('服务器未准备就绪：%s' % (svBack))
+            sk.close()
+except Exception as err:
+    print('客户端文件准备失败：%s' % (err))
+    msg = 'onFail'
+    sk.send(msg.encode())
+    sk.close()
