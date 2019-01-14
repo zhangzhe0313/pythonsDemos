10777bf22aca254cd13c163dfce9bb644bd9bc77
zhangzhe0313 <m13817158634@163.com>
2019-01-14 22:26:58 +0800

    装饰器联系

--- bibao/bibao.py
+++ bibao/bibao.py
@@ -20,3 +20,30 @@ while True:
     if not v:
         break
 print(m)
+
+
+# 闭包
+def my_sum(*arg):
+    return sum(arg)
+
+def dec(func):
+    def innerCheck(*arg):
+        if len(arg) == 0:
+            return 0
+        for s in arg:
+            if not isinstance(s, int):
+                return 0
+        return func(*arg)
+    return innerCheck
+
+my_sum = dec(my_sum)
+print('my_sum: %d' % my_sum(1, 2, 3, 4, 5, '6'))
+print('my_sum: %d' % my_sum(1, 2, 3, 4, 5))
+
+
+# 装饰器---返回一个函数，被所定义的函数接收，这里被my_newsum所接收
+@dec
+def my_newsum(*args):
+    return sum(args)
+
+print('my_newsum: %d' % my_newsum(1, 2, 3, 4, 5, 6, 7, 8, 9))
