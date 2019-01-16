864a009fe00a7609cdba0ba9c8b817b7942a1bbe
zhangzhe <zhangzhe@minshenglife.com>
2019-01-16 18:11:41 +0800

    2019-01-16

--- test.py
+++ test.py
@@ -6,3 +6,9 @@ from packages import *
 if __name__ == '__main__':
     print(packages.Circle)
     print(packages.Rectangle)
+
+    import datetime
+    d = datetime.date.weekday(datetime.date.today())
+    c = datetime.date.isoweekday(datetime.date.today())
+    print(d, c)
+    print(datetime.datetime.now())
