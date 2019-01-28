315a056be2a1c88f43e6ca0b4cebe9097ab19bf8
zhangzhe <zhangzhe@minshenglife.com>
2019-01-28 18:08:09 +0800

    汉诺塔递归

--- test.py
+++ test.py
@@ -3,6 +3,22 @@
 
 from packages import *
 
+
+def hanno(n, a, b, c):
+    '''
+    汉诺塔递归
+    :param n: 盘子总数
+    :param a: 起始塔
+    :param b: 中间塔
+    :param c: 目标塔
+    :return:
+    '''
+    if n == 0:
+        return
+    hanno(n-1, a, c, b)
+    print(a, '---->', c)
+    hanno(n-1, b, a, c)
+
 if __name__ == '__main__':
     print(packages.Circle)
     print(packages.Rectangle)
@@ -14,3 +30,11 @@ if __name__ == '__main__':
     print(datetime.datetime.now())
 
     print('id[a]', id('a')) # id() 获取内存地址
+
+    a = 'A'
+    b = 'B'
+    c = 'C'
+    n = 3
+    hanno(n, a, b, c)
+
+
